0c10332c6e309e9ccce666dc4c14288d2d1fce6b
rja87sd <rja87.sd@gmail.com>
2024-03-31 18:15:48 -0500

    Create capitalize and lowercase functions

---
+++ string-utils.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+const lowercase = (str) => str.toLowerCase();
+
+module.exports = { capitalize, lowercase };
